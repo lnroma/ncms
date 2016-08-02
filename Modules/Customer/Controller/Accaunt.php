11a3ddb66715409272481cda9723dbeb3ff954b5
lnroma <family_89@mail.ru>
2016-08-02 17:55:08 +0300

    fix error save accaunt

--- Modules/Customer/Controller/Accaunt.php
+++ Modules/Customer/Controller/Accaunt.php
@@ -7,6 +7,8 @@
  */
 namespace Customer\Controller {
 
+    use Ratchet\Wamp\Exception;
+
     class Accaunt extends \Core\Controller\AbstractClass
     {
         /**
@@ -14,37 +16,44 @@ namespace Customer\Controller {
          */
         public function saveAction()
         {
+            try {
+                $urlImage = '';
+                if (isset($_FILES['file'])) {
+                    $directory = \Core\App::getRootPath() . 'uploads' . DIRECTORY_SEPARATOR . $_SESSION['customer_id'];
+                    if (!file_exists($directory)) {
+                        mkdir($directory);
+                    }
+                    if ($_FILES['file']['error'] == 0) {
+                        move_uploaded_file($_FILES['file']['tmp_name'],
+                            $directory . DIRECTORY_SEPARATOR . $_FILES['file']['name']);
+                    }
+                    $urlImage = \Core\App::getBaseUrl() . 'uploads/' . $_SESSION['customer_id'] . '/' . $_FILES['file']['name'];
+                }
 
-            $urlImage = '';
-            if(isset($_FILES['file'])) {
-                $directory = \Core\App::getRootPath().'uploads'.DIRECTORY_SEPARATOR.$_SESSION['customer_id'];
-                if(!file_exists($directory)) {
-                    mkdir($directory);
+                $customer = \Core\Model\Mongo::simpleSelect('id', $_SESSION['customer_id'], 'customer');
+
+                $customerArr = json_decode(json_encode($customer->toArray()), true);
+                $customerArr = reset($customerArr);
+                $arrayRes = array_merge($customerArr, $_POST);
+                $arrayRes['avatar'] = $urlImage;
+                unset($arrayRes['_id']);
+
+                \Core\Model\Mongo::update($arrayRes, 'customer',
+                    array(
+                        'id' => $_SESSION['customer_id']
+                    )
+                );
+
+                $_POST['id'] = $_SESSION['customer_id'];
+
+                header('Location:' . \Core\Helper::getUrl('customer\index\index'));
+            } catch (\Exception $error) {
+                if($error->getMessage() == 'norepl') {
+                    header('Location:' . \Core\Helper::getUrl('customer\index\index'));
+                } else {
+                    throw new \Exception($error->getMessage());
                 }
-                if($_FILES['file']['error'] == 0) {
-                    move_uploaded_file($_FILES['file']['tmp_name'],
-                        $directory.DIRECTORY_SEPARATOR.$_FILES['file']['name']);
-                }
-                $urlImage = \Core\App::getBaseUrl().'uploads/'.$_SESSION['customer_id'].'/'.$_FILES['file']['name'];
             }
-
-            $customer = \Core\Model\Mongo::simpleSelect('id', $_SESSION['customer_id'],'customer');
-
-            $customerArr = json_decode(json_encode($customer->toArray()),true);
-            $customerArr = reset($customerArr);
-            $arrayRes = array_merge($customerArr,$_POST);
-            $arrayRes['avatar'] = $urlImage;
-            unset($arrayRes['_id']);
-
-            \Core\Model\Mongo::update($arrayRes,'customer',
-                array(
-                    'id' => $_SESSION['customer_id']
-                )
-            );
-
-            $_POST['id'] = $_SESSION['customer_id'];
-
-            header('Location:'.\Core\Helper::getUrl('customer\index\index'));
         }
 
     }
