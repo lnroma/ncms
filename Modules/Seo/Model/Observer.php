08e998f6929a0c4d1d43d0e9ec36e8cfaf041388
lnroma <family_89@mail.ru>
2016-08-15 20:05:39 +0300

    seo edited

--- Modules/Seo/Model/Observer.php
+++ Modules/Seo/Model/Observer.php
@@ -14,17 +14,26 @@ namespace Seo\Model {
             $request = \Core\App::getParams();
             $key = $request['controller'].'/'.$request['controllerName'].'/'.$request['action'];
 
+            $key2 = $_SERVER['REQUEST_URI'];
+
             $seoOptions = \Core\Model\Mongo::simpleSelect('page_url',$key,\Seo\Model\Entity::COLLECTION);
             $seo = $seoOptions->toArray();
 
             if(isset($seo[0])) {
                 $metaInformation['title'] = $seo[0]->title;
                 $metaInformation['description'] = $seo[0]->description;
+
+                $seoOptions = \Core\Model\Mongo::simpleSelect('page_url',$key2,\Seo\Model\Entity::COLLECTION);
+                $seo = $seoOptions->toArray();
+
+                if(isset($seo[0])) {
+                    $metaInformation['title'] = $seo[0]->title;
+                    $metaInformation['description'] = $seo[0]->description;
+                }
             }
             // todo your logical for meta information
             return $metaInformation;
         }
 
     }
-
 }
